6b29fe6a0bd0aaa39873f1dabada0a73617fb5d9
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-04-26 11:48:16 -0700

    Primer mensaje

---
+++ index2.js
@@ -0,0 +1,21 @@
+//llamamos el modulo http
+const http = require ('http');
+//establecemos la url o ip de nuestro servidor
+const hostname= '192.168.50.158';
+//establecemos el puerto de eescucha
+const port =3000;
+//creamos una sinstancia http con un reques y un response
+const server = http.createServer ((req, res) => {
+//el servidor respodnera con un codigo 200
+res.statusCode =200;
+//el servidor respondera con un texto plano
+//res.statusHeader ('Content-Type','text/plain');
+res.statusHeader ('Content-Type','text/html');
+//el servidor respodnera el mensaje hola mundo
+//res.end('Hola mundo \n');
+res.end('<h1>Hola Mundo </h1>');
+});
+server.listen(port, hostname, ()=>{
+    console.log(`El servidor se esta ejecutando en http://${hostname}:${port}/`);
+
+});
