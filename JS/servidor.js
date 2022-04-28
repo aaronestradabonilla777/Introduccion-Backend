32badd7f5a915a417a97dcb2ed9f4109be06a719
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-04-28 10:53:21 -0700

    permite abrir el error 404

--- JS/servidor.js
+++ JS/servidor.js
@@ -21,3 +21,24 @@ var mimeTypes = {
 //'.gif': 'text/gif',
 //'.mp4': 'text/mp4',
 }});
+fs.readFile(filepath, function (error,content){
+    if(error){
+        if(error.code =='ENOENT'){
+            fs.readFile('./404.html',function(error, content){
+                response.writeHead (200, { 'Content-Type':contentType});
+                response.end(content, 'utf-8');
+            });
+        }
+        else{
+            response.writeHead(500);
+            response.end('Sorry, check with the site admin for error: '+error.
+            response.end();
+        }
+    }
+    else{
+        reponse.writeHead(200,{ 'Content-Type':contentType});
+        response.end(content,'utf-8');
+    }
+});
+.listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
